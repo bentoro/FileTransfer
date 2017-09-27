808f8b2ecdb7fac29fc39356216516f539e2964f
bentoro <bl_04@hotmail.com>
2017-09-27 16:55:52 -0700

    All functions working

--- test/clt.java
+++ test/clt.java
@@ -2,40 +2,59 @@ import java.io.*;
 import java.net.*;
 
 public class clt {
-  public static void main(String args[]) throws UnknownHostException, IOException {
+  private static Socket socket;
+	private static ServerSocket socket1;
+	private static Socket clt;
+	public static void main(String args[]) throws UnknownHostException, IOException {
 
-		  //creates a socket connecting to the server
-		  Socket socket = new Socket("LOCALHOST",4444);
-		  //Socket socket = new Socket("192.168.0.17",7005);
-		  System.out.println("======================================");
-		  System.out.println("Connected to 127.0.0.1 and port 7005!");
-		  System.out.println("======================================");
+		  cmdSocketStart();
+		  dataSocketStart();
 
+
+
+		  //Read commands from client
 		  BufferedReader input = new BufferedReader (new InputStreamReader(System.in));
+		  //Store input to string
 		  String msg = input.readLine();
+
+		  //Send command to server
 		  OutputStream ots = socket.getOutputStream();
 		  DataOutputStream out = new DataOutputStream(ots);
 		  out.writeUTF(msg);
-		  //if GET commmand is used
-		  if (msg.matches("SEND")){
-		    //creates a server socket
-		    ServerSocket socket1 = new ServerSocket(7005);
-		    System.out.println("waiting for connection on port 7005");
-		    Socket clt = socket1.accept();
-		    System.out.println("CONNECTED");
 
-		    OutputStream outsvr = clt.getOutputStream();
-		    DataOutputStream outs = new DataOutputStream(outsvr);
-		    String file = "totoro.jpg";
-		    System.out.println("File name:" + file);
-		    FileInputStream fis = new FileInputStream(file);
-		    byte[] buffer = new byte[4096];
-		    //copy file to output
-		    while (fis.read(buffer)>0) {
-		      outs.write(buffer);
+
+
+		  //if GET command is used
+		  if (msg.matches("GET")){
+		    //creates a server socket
+
+		    DataInputStream in = new DataInputStream(clt.getInputStream());
+		    FileOutputStream file1 = new FileOutputStream("totoro1.jpg");
+		    byte [] buff = new byte[4096];
+		    int filesize = 15123;
+		    int read = 0;
+		    int totalRead = 0;
+		    int remaining = filesize;
+
+		    while((read = in.read(buff, 0, Math.min(buff.length, remaining))) > 0) {
+		      totalRead += read;
+		      remaining -= read;
+		      System.out.println("read " + totalRead + " bytes.");
+		      file1.write(buff, 0, read);
 		    }
 
-		  }
+		  } else if(msg.matches("SEND")){
+			  	OutputStream ops = clt.getOutputStream();
+			  	DataOutputStream outs = new DataOutputStream(ops);
+			  	String file = "totoro.jpg";
+			    System.out.println("File name:" + file);
+			    FileInputStream fis = new FileInputStream(file);
+			    byte[] buffer = new byte[4096];
+			    //copy file to output
+			    while (fis.read(buffer)>0) {
+			      outs.write(buffer);
+			    }
+		}
 
 		  /*
 		  // Write data to socket
@@ -54,5 +73,21 @@ public class clt {
 		  out.close();
 		  socket.close();*/
 
+		}
+		public static void cmdSocketStart(/*String host, int port*/) throws UnknownHostException, IOException {
+			//creates a socket connecting to the server
+			  socket = new Socket("LOCALHOST",4444);
+			  //Socket socket = new Socket("192.168.0.17",7005);
+			  System.out.println("======================================");
+			  System.out.println("Connected to 127.0.0.1 and port 7005!");
+			  System.out.println("======================================");
+		}
+
+		public static void dataSocketStart(/*int port*/) throws IOException {
+			//second socket
+			  socket1 = new ServerSocket(7005);
+			  System.out.println("waiting for connection on port 7005");
+			  clt = socket1.accept();
+			  System.out.println("CONNECTED");
 		}
 		}
--- test/svr.java
+++ test/svr.java
@@ -2,16 +2,13 @@ import java.io.*;
 import java.net.*;
 
 public class svr {
-  public static void main(String args[]) throws IOException, ClassNotFoundException {
-		  System.out.println("Connecting to port 7005");
-		  ServerSocket svr = new ServerSocket(4444);
-		  //ServerSocket svr = new ServerSocket(7005);
-		  System.out.println("======================================");
-		  System.out.println("Waiting for client to connect!");
-		  Socket socket = svr.accept();
-		  System.out.println("======================================");
-		  System.out.println("Client has connected to the server");
-		  System.out.println("======================================");
+  private static ServerSocket svr;
+	private static Socket socket;
+	private static Socket clt;
+	public static void main(String args[]) throws IOException, ClassNotFoundException {
+			cmdSocketStart();
+			dataSocketStart();
+
 
 		  //clients input stream
 		  InputStream ofs = socket.getInputStream();
@@ -20,36 +17,43 @@ public class svr {
 		  String msg = in.readUTF();
 		  System.out.println("message from client:" + msg);
 
-		  if(msg.matches("SEND")){
-		    //Create new socket
-		    Socket clt = new Socket("127.0.0.1",7005);
-		    System.out.println("Connected to 127.0.0.1 and port 4444!");
+		  OutputStream outsvr = clt.getOutputStream();
+		  DataOutputStream out = new DataOutputStream(outsvr);
 
-		    DataInputStream insvr = new DataInputStream(clt.getInputStream());
-		    FileOutputStream file = new FileOutputStream("totoro1.jpg");
-		    byte [] buffer = new byte[4096];
-		    int filesize = 15123;
-		    int read = 0;
-		    int totalRead = 0;
-		    int remaining = filesize;
-
-		    while((read = insvr.read(buffer, 0, Math.min(buffer.length, remaining))) > 0) {
-		      totalRead += read;
-		      remaining -= read;
-		      System.out.println("read " + totalRead + " bytes.");
-		      file.write(buffer, 0, read);
-		    }
-
-
-		  }
 
 		  if(msg.matches("GET")){
-		    Socket clt = new Socket("127.0.0.1",7005);
-		    System.out.println("Connected to 127.0.0.1 and port 4444!");
-		    OutputStream outsvr = clt.getOutputStream();
-		    DataOutputStream out = new DataOutputStream(outsvr);
-		  }
+		    //Create new socket
 
+		    String file = "totoro.jpg";
+		    System.out.println("File name:" + file);
+		    FileInputStream fis = new FileInputStream(file);
+		    byte[] buffer = new byte[4096];
+		    //copy file to output
+		    while (fis.read(buffer)>0) {
+		      out.write(buffer);
+		    }
+		  } else if(msg.matches("SEND")){
+
+			  	try {
+					DataInputStream ins = new DataInputStream(clt.getInputStream());
+					FileOutputStream file1 = new FileOutputStream("totoro1.jpg");
+					byte [] buff = new byte[4096];
+					int filesize = 15123;
+					int read = 0;
+					int totalRead = 0;
+					int remaining = filesize;
+
+					while((read = ins.read(buff, 0, Math.min(buff.length, remaining))) > 0) {
+					  totalRead += read;
+					  remaining -= read;
+					  System.out.println("read " + totalRead + " bytes.");
+					  file1.write(buff, 0, read);
+					}
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		}
 		  /*
 		  DataInputStream in = new DataInputStream(socket.getInputStream());
 		  FileOutputStream file = new FileOutputStream("totoro1.jpg");
@@ -67,10 +71,30 @@ public class svr {
 		  }*/
 
 
+
+		}
+	public static void cmdSocketStart(/*String host, int port*/) throws UnknownHostException, IOException {
+		 System.out.println("Connecting to port 7005");
+		  svr = new ServerSocket(4444);
+		  //ServerSocket svr = new ServerSocket(7005);
+		  System.out.println("======================================");
+		  System.out.println("Waiting for client to connect!");
+		  socket = svr.accept();
+		  System.out.println("======================================");
+		  System.out.println("Client has connected to the server");
+		  System.out.println("======================================");
+	}
+
+	public static void dataSocketStart(/*int port*/) throws IOException {
+		  clt = new Socket("127.0.0.1",7005);
+		  System.out.println("Connected to 127.0.0.1 and port 4444!");
+		  System.out.println("======================================");
+	}
+	public static void disconnect() throws IOException {
 		  System.out.println("Closing connection");
-		  in.close();
+		  //in.close();
 		  //out.close();
 		  svr.close();
 		  socket.close();
-		}
+	}
 		}
